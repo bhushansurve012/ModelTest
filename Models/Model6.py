79878ad769d439eb3db15f25c65ecb8b2ade682b
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-09-08 17:31:39 +0530

    Update Model6.py

--- Models/Model6.py
+++ Models/Model6.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def app():
-    st.title('Model6')
+    st.title('PTO')
 
-    st.write('This is the `home page` of this multi-page app.')
+    st.write('This is the `PTO` of this multi-page app.')
 
     st.write('In this app, we will be building a simple classification model using the Iris dataset.')
