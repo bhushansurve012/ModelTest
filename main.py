92e590e1aa3df38745f3ea22bf202221166653b4
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-09-08 17:34:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ st.sidebar.image("Image/logo.png",caption="64 Squares LLC")
 
 
 # Add all your application here
-app.add_app("Model1", Model1.app)
-app.add_app("Model2", Model2.app)
-app.add_app("Model3", Model3.app)
-app.add_app("Model4", Model4.app)
-app.add_app("Model5", Model5.app)
-app.add_app("Model6", Model6.app)
+app.add_app("Medication Adherence", Model1.app)
+app.add_app("NPS", Model2.app)
+app.add_app("Obesity", Model3.app)
+app.add_app("Prior Auth Final", Model4.app)
+app.add_app("Prod Isolation", Model5.app)
+app.add_app("PTO", Model6.app)
 # The main app
 app.run()
 
